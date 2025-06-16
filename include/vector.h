8dc6027f5becde3049c6680908c0681e8c6a9c13
irvin <irvin.avalos213@gmail.com>
2025-06-15 17:14:09 -0700

    added vector operations

--- include/vector.h
+++ include/vector.h
@@ -10,8 +10,8 @@ typedef struct {
   float *ele;
 } vector;
 
-#define VEC_GET_AT(vec, idx) (vec.ele[idx]);
-#define VEC_SET_AT(vec, idx, val) (vec.ele[idx] = val);
+#define VEC_GET_AT(vec, idx) vec.ele[idx]
+#define VEC_SET_AT(vec, idx, val) vec.ele[idx] = val
 
 vector vec_alloc(size_t dim);
 
@@ -22,5 +22,15 @@ float vec_get(vector vec, size_t idx);
 void vec_set(vector vec, size_t idx, float val);
 
 void vec_print(vector vec);
+
+vector vec_ones(size_t dim);
+
+void vec_scale(vector vec, float val);
+
+void vec_add(vector dst, vector src);
+
+void vec_sub(vector dst, vector src);
+
+vector vec_dot(vector dst, vector src);
 
 #endif // !VECTOR_H
--- src/vector.c
+++ src/vector.c
@@ -55,3 +55,75 @@ void vec_set(vector vec, size_t idx, float val) {
   }
   VEC_SET_AT(vec, idx, val);
 }
+
+vector vec_ones(size_t dim) {
+  if (dim <= 0) {
+    printf("Invalid dimension, %zu must be greater than or equal to 0", dim);
+    exit(EXIT_FAILURE);
+  }
+  vector vec = vec_alloc(dim);
+  for (size_t i = 0; i < dim; i++) {
+    VEC_SET_AT(vec, i, 1.0);
+  }
+  return vec;
+}
+
+void vec_scale(vector vec, float val) {
+  if (vec.ele == NULL) {
+    printf("Input vector must not be empty");
+    exit(EXIT_FAILURE);
+  }
+  for (size_t i = 0; i < vec.dim; i++) {
+    VEC_SET_AT(vec, i, VEC_GET_AT(vec, i) * val);
+  }
+}
+
+void vec_add(vector dst, vector src) {
+  if (dst.ele == NULL) {
+    printf("Destination vector must not be empty");
+    exit(EXIT_FAILURE);
+  } else if (src.ele == NULL) {
+    printf("Source vector must not be empty");
+    exit(EXIT_FAILURE);
+  } else if (dst.dim != src.dim) {
+    printf("Both vectors must have the same dimension");
+    exit(EXIT_FAILURE);
+  }
+  for (size_t i = 0; i < dst.dim; i++) {
+    VEC_SET_AT(dst, i, VEC_GET_AT(dst, i) + VEC_GET_AT(src, i));
+  }
+}
+
+void vec_sub(vector dst, vector src) {
+  if (dst.ele == NULL) {
+    printf("Destination vector must not be empty");
+    exit(EXIT_FAILURE);
+  } else if (src.ele == NULL) {
+    printf("Source vector must not be empty");
+    exit(EXIT_FAILURE);
+  } else if (dst.dim != src.dim) {
+    printf("Both vectors must have the same dimension");
+    exit(EXIT_FAILURE);
+  }
+  for (size_t i = 0; i < dst.dim; i++) {
+    VEC_SET_AT(dst, i, VEC_GET_AT(dst, i) - VEC_GET_AT(src, i));
+  }
+}
+
+vector vec_dot(vector vec1, vector vec2) {
+  if (vec1.ele == NULL) {
+    printf("Destination vector must not be empty");
+    exit(EXIT_FAILURE);
+  } else if (vec2.ele == NULL) {
+    printf("Source vector must not be empty");
+    exit(EXIT_FAILURE);
+  } else if (vec1.dim != vec2.dim) {
+    printf("Both vectors must have the same dimension");
+    exit(EXIT_FAILURE);
+  }
+  vector ret = vec_alloc(vec1.dim);
+  for(size_t i = 0; i < ret.dim; i++) {
+    VEC_SET_AT(ret, i, VEC_GET_AT(vec1, i) * VEC_GET_AT(vec2, i));
+  }
+  return ret;
+}
