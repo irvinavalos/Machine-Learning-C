69d5ffea7e53c4dd0afd42d2e6ea76d0c9ac5f93
irvin <irvin.avalos213@gmail.com>
2025-06-17 19:34:47 -0700

    added helper function for minst_data.c

--- src/data/mnist_data.c
+++ src/data/mnist_data.c
@@ -1,5 +1,14 @@
 #include "mnist_data.h"
 
+static uint32_t read_be32(FILE *fp) {
+  uint8_t arr[4];
+  if (fread(arr, 1, 4, fp) != 4) {
+    perror("fread");
+    exit(EXIT_FAILURE);
+  }
+  return (arr[0] << 24) | (arr[1] << 16) | (arr[2] << 8) | arr[3];
+}
+
 int mnist_load_image(const char *path, mnist_image *out);
 
 int mnist_load_label(const char *path, mnist_label *out);
