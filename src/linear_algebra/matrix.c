c97df383833399e8e9083f8ee3ef5fe70f2458ba
irvin <irvin.avalos213@gmail.com>
2025-06-17 21:20:24 -0700

    added mat_status enum for error handling

--- src/linear_algebra/matrix.c
+++ src/linear_algebra/matrix.c
@@ -1,5 +1,24 @@
 #include "matrix.h"
 
+const char *mat_strerror(mat_status stat) {
+  switch (stat) {
+  case MAT_OK:
+    return "success";
+  case MAT_ERR_OOB:
+    return "index out of bounds";
+  case MAT_ERR_SHAPE:
+    return "shape mismatch";
+  case MAT_ERR_ALLOC:
+    return "memory allocation failed";
+  case MAT_ERR_NULL:
+    return "NULL pointer argument";
+  case MAT_ERR_SINGULAR:
+    return "matrix is singular / not invertible";
+  default:
+    return "unknown matrix error";
+  }
+}
+
 matrix mat_alloc(size_t row, size_t col) {
   matrix mat;
   mat.row = row;
